9c40d2c9feedd44589b99b0a8cacee6c184c8f2b
shakibh10~ <billalsikdar03@gmail.com~>
2024-09-20 08:50:18 +0600

    Second commit

---
+++ user-input.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    string name;
+
+    cout << "Enter the name:  ";
+    cin >> name;
+
+    cout << "The name is: " << name;
+
+
+
+    int num=29;
+    float x=2.99;
+    double y=3.99;
+    bool myBool=true;
+    string herName="Bushra";
+
+    cout << num << " " <<  y  << " "  <<myBool << " " << herName;
+
+
+
+    return 0;
+}
